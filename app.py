c2d201859a8567fd192a57c2651cc84835d08890
axelofwar <tylercaton2350@gmail.com>
2023-01-25 21:45:17 -0500

    Discord/chatGPT bot - channel population error

--- app.py
+++ app.py
@@ -1,133 +1,107 @@
 import os
 import openai
 import asyncio
-import tkinter as tk
+import ui
+import yaml
+import discord
+from dotenv import load_dotenv
 
-#openai.api_key = os.getenv("OPENAI_API_KEY") #if using python .env file
-openai.api_key = os.environ['OPEN_API_KEY']  #if using replit Secrets function
-openai.api_endpoint = "https://api.openai.com/v1"
+load_dotenv()
+# openai.api_key = os.getenv("OPENAI_API_KEY")  # if using python .env file
+# openai.api_key = os.environ.get("OPENAI_API_KEY")  # if using python .env m2
+# if using replit Secrets function
+openai.api_key = os.environ['OPENAI_API_KEY']
+if not openai.api_key:
+    openai.api_key = ""
 
-def update_temp(value):
-  temper = float(value)
-  global temp
-  temp = temper
+with open("config.yml", "r") as file:
+    config = yaml.load(file, Loader=yaml.FullLoader)
+
+openai.api_endpoint = config["OPENAI_ENDPOINT"]
+
+# function for ChatGPT call
 
 
-def update_max_tokens(value):
-  max_token_count = float(value)
-  global max_token
-  max_token = int(max_token_count)
+async def chatGPTcall(mPrompt, mModel, mTemp, mTokens):
+    response = openai.Completion.create(
+        model=mModel,
+        prompt=mPrompt,
+        # uniqueness modifiers
+        temperature=mTemp,
+        max_tokens=mTokens,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.6,
+        stop=[" Human:", " AI:"])
+    return response
 
 
-def submit():
-  window.destroy()
-
-window = tk.Tk()
-
-screen_width = window.winfo_screenwidth()
-screen_height = window.winfo_screenheight()
-
-width = 300  #window.winfo_width()
-height = 200  #window.winfo_height()
-
-x = (screen_width / 2) - (width / 2)
-y = (screen_height / 2) - (height / 2)
-
-window.geometry("%dx%d+%d+%d" % (width, height, x, y))
-
-label = tk.Label(window, text="Temperature Slider")
-temp_slider = tk.Scale(window,
-                       from_=0,
-                       to=2,
-                       orient=tk.HORIZONTAL,
-                       resolution=0.1,
-                       command=update_temp)
-label.pack()
-temp_slider.pack()
-
-label = tk.Label(window, text="Max Tokens Slider")
-max_token_slider = tk.Scale(window,
-                            from_=0,
-                            to=150,
-                            orient=tk.HORIZONTAL,
-                            resolution=0.1,
-                            command=update_max_tokens)
-label.pack()
-max_token_slider.pack()
-
-submit_button = tk.Button(window, text="Submit", command=submit)
-submit_button.pack()
-
-#function for ChatGPT call
-async def ChatGPTCall(mPrompt, mModel, mTemp, mTokens):
-  response = openai.Completion.create(
-    model=mModel,
-    prompt=mPrompt,
-    #amount of uniqueness for each response params below
-    temperature=mTemp,
-    max_tokens=mTokens,  #bounds on the response token - TODO: clamp user input
-    top_p=1,
-    frequency_penalty=0.0,
-    presence_penalty=0.6,
-    stop=[" Human:", " AI:"])
-  return response
-
-#function for doing main chatGPT logic
 async def main():
-  if 'temp' != globals() and 'max_token' != globals():
-    temp = 0.9
-    max_token = 100
+    print("TRY TO OPEN UI.MAIN() IN APP.PY")
+    print("CHANNEL PRE UI: ", config["discord_channel_id"])
+    try:
+        channel_id, history_days = await asyncio.create_task(ui.main())
+        print("TRY CHANNEL_ID: ", channel_id)
+    except:
+        channel_id = config["discord_channel_id"]
+        history_days = 30
 
-  print("TEMP: ", temp)
-  print("MAX TOKENS: ", max_token)
-  HelloWorld = await ChatGPTCall(
-    "Hey, hope you're well today. Teach me something new about python or react!", temp, max_token)
+    print("ENTERING MAIN + CONFIG READ")
 
-  print(HelloWorld.choices[0].text)
+    prompt = config["prompt"]
+    model = config["davinci"]
 
-  filename = 'output.txt'
+    print("CHANNEL_ID PRE: ", channel_id)
+    print("DISCORD BOT ENTERED")
 
-  if os.path.exists(filename):
-    outputFile = open(filename, mode='r+')
-  else:
-    outputFile = open(filename, 'x')
+    # Create a Discord client
+    discord_token = os.getenv("DISCORD_TOKEN")  # if using python .env file
 
-  #Here we store all the response data in an output file instead of just returning the text member
-  print(HelloWorld.choices[0].text, file=outputFile)
-  outputFile.close()
+    client = discord.Client(
+        intents=discord.Intents.default())
+
+    # print("DISCORD_TOKEN: ", discord_token) # to confirm operational
+
+    try:
+        await client.start(discord_token)
+    except Exception as exc:
+        print(exc)
+        raise discord.errors.LoginFailure(
+            'Improper token has been passed.') from exc
+
+    print("DISCORD BOT STARTED")
+    if channel_id == None:
+        # use channel_id from config id
+        channel_value = int(config["discord_channel_id"])
+        print("CHANNEL_VALUE: ", channel_value)
+        channel = client.get_channel(channel_value)
+        channel_id = channel_value
+        print("CHANNEL GET SUCCESS FROM NONE: ", channel)
+
+    else:
+        print("CHANNEL VALUE RECIEVED: ", channel_id)
+        # use channel_id passed into UI
+        channel = client.get_channel(int(channel_id))
+
+    print("History: ", history_days)
+    print("CHANNEL_ID POST: ", channel_id)
+    print("CHANNEL POST: ", channel)
+
+    messages = await channel.history(limit=history_days, oldest_first=True).flatten()
+
+    print("MESSAGES: ", messages)
+
+    filename = 'output.txt'
+
+    if os.path.exists(filename):
+        outputFile = open(filename, mode='r+')
+    else:
+        outputFile = open(filename, 'x')
+
+    for message in messages:
+        outputFile.write(message.content)
+
+    outputFile.close()
 
 
-window.mainloop()
-asyncio.run(main())
-
-'''
-#OpenAI Example Query using Flask App
-from flask import Flask, redirect, render_template, request, url_for
-app = Flask(__name__)
-@app.route("/", methods=("GET", "POST"))
-def index():
-    if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
-
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
-
-
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
-'''
+asyncio.get_event_loop().run_until_complete(main())
