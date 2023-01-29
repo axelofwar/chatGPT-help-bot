72d45bfe20fcec5e2dd3d937693fedf239107415
axelofwar <tylercaton2350@gmail.com>
2023-01-28 20:20:52 -0500

    utils created

---
+++ utils/update_output_txt.py
@@ -0,0 +1,20 @@
+async def update_output_file(response):
+    # TODO: modify to use r+ for read/write and make one open call
+    with open("outputs/output.txt", "r") as outputFile:
+        lines = outputFile.readlines()
+        outputFile.close()
+
+    with open("outputs/output.txt", "w") as outputFile:
+        found_response = False
+        for line in lines:
+            if "RESPONSE: " in line:
+                if not found_response:
+                    outputFile.write(
+                        "RESPONSE: " + response.choices[0].text + "\n")
+                    found_response = True
+            else:
+                outputFile.write(line)
+        if not found_response:
+            outputFile.write("RESPONSE: " + response.choices[0].text + "\n")
+    outputFile.close()
+    return "OUTPUT FILE UPDATED"
