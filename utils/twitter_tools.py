d60c899b717b9241b6ff503148adbd983bcc7c5b
axelofwar <tylercaton2350@gmail.com>
2023-01-29 01:31:42 -0500

    add twitter listener -> sleep -> disconnect

--- utils/twitter_tools.py
+++ utils/twitter_tools.py
@@ -1,11 +1,18 @@
 import tweepy
 import os
 import yaml
+import asyncio
+from twitter_stream import FilteredStream
 
 with open("utils/config.yml", "r") as file:
     config = yaml.load(file, Loader=yaml.FullLoader)
 
 running = True
+# myStream = tweepy.Stream(os.getenv("TWITTER_BEARER_TOKEN"), os.getenv("TWITTER_API_KEY"), os.getenv(
+#     "TWITTER_ACCESS_TOKEN"), os.getenv("TWITTER_ACCESS_TOKEN_SECRET"))
+
+myStream = tweepy.StreamingClient(
+    os.getenv("TWITTER_BEARER_TOKEN"))
 
 
 async def initTwitter():  # function for initializing Twitter API
@@ -18,19 +25,42 @@ async def initTwitter():  # function for initializing Twitter API
     return api
 
 
-async def call_listener(api, account, cancel):
+async def call_once(api, account, cancel):
     if not cancel:
-        # get all mentions of [account_to_query] in [tweet_history] days
         tweets = api.search_tweets(
             q=account, count=config["tweet_history"])
+        print("TWEET HISTORY CALLED ONCE")
         return tweets
     else:
-        running = False
         print("CANCELLED")
-        return running
+        return cancel
+
+
+async def init_listener(api, account, cancel):
+    # mentions of [account_to_query]
+    # myStream.filter(track=["@"+account], callback=on_tweet)
+    # api.filter(track=["@"+account], callback=on_tweet)
+    # myStream.filter(track=["@"+account])
+    myStream.on_data = on_tweet
+    print("STREAMING TWEETS", myStream)
+    await asyncio.sleep(5)
+    running = False
+    print("STREAMING CANCELLED")
+    return running
+
+
+async def on_tweet(tweet):
+    print("TWEET RECEIVED")
+    print(tweet.text)
+    print(tweet.user.screen_name)
+    print(tweet.created_at)
+    print(f"https://twitter.com/{tweet.user.screen_name}/status/{tweet.id}")
+    return tweet
 
 
 async def closeListener():
+    myStream.disconnect()
+    print("STREAMING DISCONNECTED")
     running = False
 
 
