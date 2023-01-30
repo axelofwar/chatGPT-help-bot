04984d7c7b4ffa88fa8a5040b531422f30c94494
axelofwar <tylercaton2350@gmail.com>
2023-01-29 22:17:03 -0500

    Upgrade app + add tools

--- app.py
+++ app.py
@@ -1,32 +1,15 @@
-import os
-import openai
 import asyncio
 import sys
 import yaml
-import discord
-import tweepy
 import urllib.parse
-import re
-# from collections import Counter
+from utils import mod as tools
 from dotenv import load_dotenv
 
-from utils import ui
-from utils import twitter_tools as th
-from utils import discord_tools as dh
-from utils import update_output_txt as uout
 
 load_dotenv()
-# openai.api_key = os.getenv("OPENAI_API_KEY")  # if using python .env file
-# if using replit Secrets function
-openai.api_key = os.environ['OPENAI_API_KEY']
-if not openai.api_key:
-    openai.api_key = ""
-    print("OPENAI API KEY NOT FOUND")
 
 with open("utils/config.yml", "r") as file:
     config = yaml.load(file, Loader=yaml.FullLoader)
-
-openai.api_endpoint = config["OPENAI_ENDPOINT"]
 
 # get config params
 prompt = config["prompt"]
@@ -35,140 +18,64 @@ temp = config["temp"]
 max_tokens = config["max_tokens"]
 
 
-async def chatGPTcall(mPrompt, mModel, mTemp, mTokens):  # function for ChatGPT call
-    response = openai.Completion.create(
-        model=mModel,
-        prompt=mPrompt,
-        # uniqueness modifiers
-        temperature=mTemp,
-        max_tokens=mTokens,
-        top_p=1,
-        frequency_penalty=0.0,
-        presence_penalty=0.6,
-        stop=[" Human:", " AI:"])
-    return response
-
-
-# function for intializing Discord client and getting desired channel history
-async def get_channel_history(channel_id, history_days, cancel):
-
-    print("DISCORD BOT ENTERED")
-    # # Create a Discord client
-    if not cancel:
-        client = await dh.init_discord(cancel)
-        discord_token = os.getenv("DISCORD_TOKEN")  # if using python .env file
-
-    else:
-        print("CANCELLED")
-        return cancel
-
-    try:
-        # start discord client
-        # TODO: improve logic so that client closes on complete instead of timeout required
-        if not cancel:
-            task = asyncio.create_task(client.start(discord_token))
-            await asyncio.wait_for(task, config["timeout"])
-        else:
-            print("TASK CANCELLED")
-            sys.exit()
-
-        # TODO: modify to display this info properly
-        print("Client User: ", client.user)
-        print(f'{client.user} status: {client.status}')
-        print(f'{client.user} is connected to the following guild:')
-        print(f'{client.user} current activity: {client.activity}')
-        print("CLIENT STARTED SUCCESSFULLY", client.status)
-    except asyncio.TimeoutError:
-        print("CLIENT TIMEOUT")
-    except discord.errors.LoginFailure as exc:
-        print(f'Error: {exc}')
-    except discord.errors.ClientException as exc1:
-        print(f'Error: {exc1}')
-    except discord.errors.HTTPException as exc2:
-        print(f'Error: {exc2}')
-    except discord.errors.DiscordException as exc4:
-        print(f'Error: {exc4}')
-
-    # TODO: add more channels to this list
-    print("DISCORD BOT STARTED")
-    if channel_id == None:
-        # use channel_id from config id
-        channel_value = int(config["discord_channel_id"])
-        print("CHANNEL_VALUE: ", channel_value)
-        channel = client.get_channel(channel_value)
-        channel_id = channel_value
-        print("CHANNEL GET SUCCESS FROM NONE: ", channel)
-
-    else:
-        print("CHANNEL VALUE RECIEVED: ", channel_id)
-        # use channel_id passed into UI
-        try:
-            channel = client.get_channel(int(channel_id))
-            print("CHANNEL GET SUCCESS: ", channel)
-
-        except:
-            print("CHANNEL GET FAILED")
-            sys.exit()
-
-    print("CHANNEL HISTORY: ", history_days)
-    print("CHANNEL_ID POST: ", channel_id)
-    print("CHANNEL POST: ", channel)
-
-    channel_history = channel.history(limit=history_days, oldest_first=True)
-
-    # done with connection to discord
-    # task.cancel()
-    # TODO: fix cancel on complete so I don't need to wait whole 15 seconds
-    return channel, channel_history
-
-
 async def main():
     # init twitter API
-    twitterAPI = await th.initTwitter()
+    twitterAPI = await tools.th.init_twitter()
 
     try:
-        task = asyncio.create_task(ui.main())
-        channel_id, history_days, cancel = await task
+        task = asyncio.create_task(tools.ui.main())
+        data_channel_id, history_days, cancel = await task
         print("UI SUCCESS: APP.PY USED UI VALUES")
-        print("UI CHANNEL_ID: ", channel_id)
+        print("UI CHANNEL_ID: ", data_channel_id)
         print("CANCEL: ", cancel)
 
-        runner = await th.init_listener(twitterAPI, config["account_to_query"], cancel)
+        # myStream, runner = await th.init_listener(twitterAPI, config["account_to_query"], cancel)
 
         # open file to write to
         with open("outputs/tweets.txt", "w") as tweetFile:
-            search_results = await th.printTweetHistory(tweets, tweetFile)
-        print("RUNNER: ", runner)
+            search_results = await tools.th.print_tweet_history(tweets, tweetFile)
+        # print("RUNNER: ", runner)
     except:
         print("UI FAILED/EMPTY: APP.PY USED DEFAULTS")
-        channel_id = config["discord_channel_id"]
-        print("EXCEPT CHANNEL_ID: ", channel_id)
+        data_channel_id = config["data_channel_id"]
+        print("EXCEPT CHANNEL_ID: ", data_channel_id)
         print("EXCEPT CANCEL: ", cancel)
         history_days = 30
-
     # get channel & history from UI or defaults - modify to pass params?
     if not cancel:
-        channel, channel_history = await get_channel_history(channel_id, history_days, cancel)
+        data_channel, data_channel_history = await tools.dh.get_channel_history(data_channel_id, history_days, cancel)
+        chat_channel, chat_channel_history = await tools.dh.get_channel_history(config["chat_channel_id"], 5, cancel)
 
-    while th.running:
-        tweets = await th.call_once(
+    # while th.running:
+        tweets = await tools.th.call_once(
             twitterAPI, config["account_to_query"], cancel)
+
+        # open file to write to
+        with open("outputs/tweets.txt", "w") as tweetFile:
+            search_results = await tools.th.print_tweet_history(tweets, tweetFile)
+
+        # myStream, runner = await th.init_listener(twitterAPI, config["account_to_query"], cancel)
 
         # TODO: clean file read/write logic so updates can be written and read from same opened instance
         with open("outputs/discord.txt", "w") as outputFile:
-            # print chanel history to file
-            discord_history = await dh.printDiscordHistory(channel_history, outputFile)
+            # print chanel history to file for DATA channel
+            data_messages, discord_history = await tools.dh.get_discord_messages(data_channel_history, outputFile)
             # returns list of message objects
+
         with open("outputs/discord.txt", "r") as outputFile:
             discord_history = outputFile.read()
             # returns string of discord history
-        mprompt = prompt + \
-            f"Use primarily the data in this file to answer:\n{discord_history}\n"
 
-        GPTresponse = await chatGPTcall(mprompt, model, temp, max_tokens)
+        question_chat = await tools.dh.get_questions(chat_channel_history)
+        # print("QUESTION_CHAT: ", question_chat[0])
+
+        prompt_tag = f"Use primarily the data in this file to answer it:\n{discord_history}\n"
+        mprompt = prompt + prompt_tag
+
+        GPTresponse = await tools.gpt.chatGPTcall(mprompt, model, temp, max_tokens)
         print("RESPONSE: ", GPTresponse.choices[0].text)
 
-        running = updateOutput = await uout.update_output_file(GPTresponse)
+        running = updateOutput = await tools.uout.update_output_file(GPTresponse)
         # prints response to terminal + output file
 
         # TODO: do stuff with search_results from twitter + channel & channel_history from discord
@@ -176,9 +83,9 @@ async def main():
         print("\n", "RUNNING: ", running)
         # return "SUCCESS"
 
-    if cancel or not th.running:
+    if cancel or not tools.th.running:
         print("ENDED")
-        await th.closeListener()
+        # await th.close_listener(myStream)
         sys.exit()
     return sys.exit()
 
---
+++ utils/chat_gpt_tools.py
@@ -0,0 +1,30 @@
+import openai
+import os
+import yaml
+
+from dotenv import load_dotenv
+load_dotenv()
+
+with open("utils/config.yml", "r") as file:
+    config = yaml.load(file, Loader=yaml.FullLoader)
+
+openai.api_endpoint = config["OPENAI_ENDPOINT"]
+
+openai.api_key = os.environ['OPENAI_API_KEY']
+if not openai.api_key:
+    openai.api_key = ""
+    print("OPENAI API KEY NOT FOUND")
+
+
+async def chatGPTcall(mPrompt, mModel, mTemp, mTokens):  # function for ChatGPT call
+    response = openai.Completion.create(
+        model=mModel,
+        prompt=mPrompt,
+        # uniqueness modifiers
+        temperature=mTemp,
+        max_tokens=mTokens,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.6,
+        stop=[" Human:", " AI:"])
+    return response
